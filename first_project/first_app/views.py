2218be3fe7d3ecab9f4ed7aae7e3ed7c80d4c984
eithiri <eithiritun1997@gmail.com>
2022-01-06 17:08:23 +0630

    feat: 122. Django Level One - Part Seven - Templates

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+# def index(request):
+#     return HttpResponse("Hello World!.And Testing git!")
+
 def index(request):
-    return HttpResponse("Hello World!.And Testing git!")
+    my_page = {'insert_me' : "Hello!. Testing by viwe for template!"}
+    return render(request,"index.html",context=my_page)
