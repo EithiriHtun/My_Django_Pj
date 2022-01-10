3080e28ce0f1e3862b228b38e20cdfb76b9c9cc3
eithiri <eithiritun1997@gmail.com>
2022-01-10 16:54:05 +0630

    feat: 130. Models-Templates-Views Paradigm

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from first_app.models import Topic,Webpage,AccessRecord
 # Create your views here.
 
 # def index(request):
 #     return HttpResponse("Hello World!.And Testing git!")
 
 def index(request):
-    my_page = {'insert_me' : "Hello!. Testing by viwe for template!"}
-    return render(request,"index.html",context=my_page)
+    webpages_list = AccessRecord.objects.order_by('date')
+    date_dict = {'access_records':webpages_list}
+    return render(request,"first_app/index.html",context=date_dict)
+
+    # my_page = {'insert_me' : "Hello!. Testing by viwe for template!"}
+    # return render(request,"index.html",context=my_page)
