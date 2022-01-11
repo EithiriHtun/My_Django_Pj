40000d3104f8f3a64bb3659bf4f70f8b8190e113
eithiri <eithiritun1997@gmail.com>
2022-01-11 09:09:24 +0630

    feat: 135. Form Basics Code Along

--- first_project/first_project/urls.py
+++ first_project/first_project/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('',views.index,name='index'),
     path('first_app/',include('first_app.urls')),
     path('admin/', admin.site.urls),
+    path('form_page/',views.form_name_view,name='form'),
 ]
