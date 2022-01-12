b1a5f55019d1eb5ae14657ecd203b4d9c1a27045
eithiri <eithiritun1997@gmail.com>
2022-01-12 14:42:12 +0630

    feat: 148. Django Passwords

--- first_project/first_project/settings.py
+++ first_project/first_project/settings.py
@@ -20,6 +20,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR,"templates")
 
 STATIC_DIR = os.path.join(BASE_DIR,"static")
 
+MEDIA_DIR = os.path.join(BASE_DIR,"media")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -92,6 +93,14 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.Argon2PasswordHAsher',
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    'django.contrib.auth.hashers.BCryptPasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+]
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -130,6 +139,11 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
    STATIC_DIR,
 ]
+
+#MEDIA_DIR
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media'
+
 
 INTERNAL_IPS = ['127.0.0.1']
 
